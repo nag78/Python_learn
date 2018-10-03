2c8fb323d7af3c3492d1a00dd3d3c8bbac99819b
Nikolay Goltsev <nag78@yandex.ru>
2018-10-03 18:36:58 +0300

    3.10.2018

---
+++ bicycles.py
@@ -0,0 +1,4 @@
+bicycles = ['trek','cannondale','redline','specialized']
+print(bicycles[0].title())
+print(bicycles[1].title())
+print(bicycles[-1].title())
---
+++ spiski_1.py
@@ -0,0 +1,22 @@
+friends = ['nadya','denis','andrey','victor']
+print (friends[-1].title())
+print (friends[0].title())
+print (friends[2].title())
+print (friends[1].title())
+
+message = "Привет, " + friends[-1].title()+"!"
+print(message)
+
+message = "Привет, " + friends[0].title()+"!"
+print(message)
+
+message = "Привет, " + friends[2].title()+"!"
+print(message)
+
+message = "Привет, " + friends[1].title()+"!"
+print(message)
+
+
+
+transport = ['Chevrolet Niva','BMW X6','SUZUKI VITARA','HONDA H-RV']
+print("Я бы хотел приобрести " + transport[0] + "!")
