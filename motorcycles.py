090a7c7375a9688236235b3a1ae72cc8ea6d6aa8
Nikolay Goltsev <nag78@yandex.ru>
2018-10-04 16:51:21 +0300

    списки

--- motorcycles.py
+++ motorcycles.py
@@ -3,6 +3,7 @@ print(motorcycles)
 
 
 #motorcycles[0] = 'ducati'
+# Добавление элемента списка в конец списка
 
 motorcycles.append('ducati')
 print(motorcycles)
@@ -24,8 +25,29 @@ print(motorcycles)
 
 
 
+# Удаление с помощью pop()
 
 poped_motorcycles = motorcycles.pop()
 print(motorcycles)
 print(poped_motorcycles)
+
+motorcycles = ['honda','yamaha','suzuki']
+last_owned = motorcycles.pop(0)
+print("Последний мотокцил который уменя был это " + last_owned.title() + ".")
+
+
+# Удаление элементов по значению
+
+motorcycles = ['honda','yamaha','suzuki','ducati']
+print(motorcycles)
+
+motorcycles.remove('ducati')
+print(motorcycles)
+
+motorcycles = ['honda','yamaha','suzuki','ducati']
+print(motorcycles)
+too_expensive = 'ducati'
+motorcycles.remove(too_expensive)
+print(motorcycles)
+print("\nA " + too_expensive.title() + " is too expensive for me. ")
 
