6158d34b1f8cd8293c7d2e03c13856ac2e1d623d
Nikolay Goltsev <nag78@yandex.ru>
2018-10-08 17:49:09 +0300

    Циклы и списки

--- animals.py
+++ animals.py
@@ -1,5 +1,5 @@
 animals = ['cat','dog','onehorn']
 for animal in animals:
-    print("А у " + animal + " четыре ноги, позади длинный хвост!\n")
+    print("А у " + animal.title() + " четыре ноги, позади длинный хвост!\n")
 print("Но трогать его немоги, за его малый рост!\n")
 print("\tМалый рост!\n")
