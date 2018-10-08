d8d9b902c97c7b941e057428e30ecf64fca3b688
Nikolay Goltsev <nag78@yandex.ru>
2018-10-08 17:48:19 +0300

    Циклы  и списки

---
+++ animals.py
@@ -0,0 +1,5 @@
+animals = ['cat','dog','onehorn']
+for animal in animals:
+    print("А у " + animal + " четыре ноги, позади длинный хвост!\n")
+print("Но трогать его немоги, за его малый рост!\n")
+print("\tМалый рост!\n")
---
+++ pizza.py
@@ -0,0 +1,4 @@
+pizzas = ['papa johns','margarita','pepperoni']
+for pizza in pizzas:
+    print("I like a " + pizza.title() + "!\n")
+print ("I really love pizza!")
