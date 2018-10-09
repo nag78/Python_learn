01da317ecd1f8f9f19f366188f463c8952d01f68
Nikolay Goltsev <nag78@yandex.ru>
2018-10-09 15:40:25 +0300

    Создание числовых списков

---
+++ even_numbers.py
@@ -0,0 +1,17 @@
+#-------------------------------------------------------------------------------
+# Name:        even_numbers
+# Purpose:
+#
+# Author:      Nag
+#
+# Created:     09.10.2018
+# Copyright:   (c) Nag 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+# Формирования числового списка
+numbers = list(range(1,6))
+print(numbers)
+
+# Четный список
+even_numbers = list(range(2,11,2))
+print(even_numbers)
---
+++ squares.py
@@ -0,0 +1,17 @@
+#-------------------------------------------------------------------------------
+# Name:        squares
+# Purpose:
+#
+# Author:      Nag
+#
+# Created:     09.10.2018
+# Copyright:   (c) Nag 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+# Возведение в степень
+
+squares = []
+for value in range(1,11):
+        squares.append(value **2)
+print(squares)
