81466db798cf5e54238f957fcafc541947ded865
Nikolay Goltsev <nag78@yandex.ru>
2018-10-12 15:47:05 +0300

    Работа со списками

--- pizza.py
+++ pizza.py
@@ -1,4 +1,20 @@
 pizzas = ['papa johns','margarita','pepperoni']
+
+friend_pizzas = pizzas[:]
+pizzas.append('mexican')
+friend_pizzas.append('gavai')
+
+print("My favorite pizzas are:")
+print(pizzas)
+
+print("My friend's favorite pizzas are:")
+print(friend_pizzas)
+
+
 for pizza in pizzas:
+    print("I like a " + pizza.title() + "!\n")
+print ("I really love pizza!")
+
+for pizza in friend_pizzas:
     print("I like a " + pizza.title() + "!\n")
 print ("I really love pizza!")
