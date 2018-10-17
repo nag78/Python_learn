7b9bde6621a2868e933bec3fbd50b1d57a645d6d
Nikolay Goltsev <nag78@yandex.ru>
2018-10-17 17:24:47 +0300

    if

---
+++ amusement_park.py
@@ -0,0 +1,22 @@
+#-------------------------------------------------------------------------------
+# Name:        amusement_park
+# Purpose:
+#
+# Author:      Nag
+#
+# Created:     17.10.2018
+# Copyright:   (c) Nag 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+age = 12
+
+if age < 4:
+    print ("Your admission cost is $0.")
+elif age < 18:
+    print ("Your admission cost is $5.")
+else:
+    print ("Your admission cost is $10.")
+
+
+print ("Your admission cost is $0.")
---
+++ voting.py
@@ -0,0 +1,18 @@
+#-------------------------------------------------------------------------------
+# Name:        voting
+# Purpose:
+#
+# Author:      Nag
+#
+# Created:     17.10.2018
+# Copyright:   (c) Nag 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+age = 17
+if age >= 18:
+    print("You are old enough to vote!")
+    print("Have you registered to vote yet?")
+else:
+    print("Sorry, you are too young to vote.")
+    print("Please register to vote as soon as you turn 18!")
