327059a875230d8bbd878527aa111c75231cb9ee
Nikolay Goltsev <nag78@yandex.ru>
2018-10-17 17:27:07 +0300

    if

--- amusement_park.py
+++ amusement_park.py
@@ -12,11 +12,14 @@
 age = 12
 
 if age < 4:
-    print ("Your admission cost is $0.")
+    price = 0
+#   print ("Your admission cost is $0.")
 elif age < 18:
-    print ("Your admission cost is $5.")
+    price = 5
+#   print ("Your admission cost is $5.")
 else:
-    print ("Your admission cost is $10.")
+    price = 10
+#    print ("Your admission cost is $10.")
 
 
-print ("Your admission cost is $0.")
+print ("Your admission cost is $" + str(price) + ".")
