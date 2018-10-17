c6f55223837b0b715747fb16ec7fb8776b9ea945
Nikolay Goltsev <nag78@yandex.ru>
2018-10-17 12:53:58 +0300

    Кортежи

---
+++ swedish_tool.py
@@ -0,0 +1,19 @@
+#-------------------------------------------------------------------------------
+# Name:        swedish_tool
+# Purpose:
+#
+# Author:      Nag
+#
+# Created:     17.10.2018
+# Copyright:   (c) Nag 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+swedish_tool = ('eggs','ananas','banan','sausage','gamburger')
+print("\nMenu:")
+for food in swedish_tool:
+    print(food)
+# swedish_tool[1] = 'pizza'
+swedish_tool = ('eggs','pizza','banan','sausage','ananas')
+print ("\nModifed Menu:")
+for food in swedish_tool:
+    print(food)
