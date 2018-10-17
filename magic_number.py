c88b83870ed5f5c9949a297f03a46d17a6800813
Nikolay Goltsev <nag78@yandex.ru>
2018-10-17 15:30:54 +0300

    Условия

---
+++ magic_number.py
@@ -0,0 +1,13 @@
+#-------------------------------------------------------------------------------
+# Name:        magic_number
+# Purpose:
+#
+# Author:      Nag
+#
+# Created:     17.10.2018
+# Copyright:   (c) Nag 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+answer = 17
+if answer != 42:
+    print("That is not correct answer. Please try again!")
---
+++ toppings.py
@@ -0,0 +1,13 @@
+#-------------------------------------------------------------------------------
+# Name:        toppings
+# Purpose:
+#
+# Author:      Nag
+#
+# Created:     17.10.2018
+# Copyright:   (c) Nag 2018
+# Licence:     <your licence>
+#-----------------------------------------------------------------------
+requested_topping = 'mushrooms'
+if requested_topping != 'anchovies':
+    print("Hold the anchovies!")
