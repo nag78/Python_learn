df8f0e7bda5e240ff6542db3882c5eaf9780b086
Nikolay Goltsev <nag78@yandex.ru>
2018-10-18 17:25:45 +0300

    if списки

--- toppings.py
+++ toppings.py
@@ -8,16 +8,23 @@
 # Copyright:   (c) Nag 2018
 # Licence:     <your licence>
 #-----------------------------------------------------------------------
-requested_topping =['mushrooms','onion','pineapple','extra_cheese']
+requested_toppings =['mushrooms','onion','pineapple','extra_cheese']
 #'mushrooms' in requested_topping
 
-if 'mushrooms' in requested_topping:
+if 'mushrooms' in requested_toppings:
 #     print("Hold the mushrooms!")
     print("Adding mushrooms.")
-if 'peperoni' in requested_topping:
+if 'peperoni' in requested_toppings:
     print("Adding peperoni")
-if 'extra_cheese' in requested_topping:
+if 'extra_cheese' in requested_toppings:
     print("Adding extra_cheese")
 
 
 print("\nFinished making your pizza!")
+
+
+requested_toppings = ['mushrooms','green pepper','extra cheese']
+
+for requested_topping in requested_toppings:
+    print("Adding " + requested_topping + ".")
+print("\nFinisher making your pizza!")
