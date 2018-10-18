83d4f7589c8b03380187f7dbdb64535d35d7016f
Nikolay Goltsev <nag78@yandex.ru>
2018-10-18 14:20:09 +0300

    if

---
+++ favorite_fruits.py
@@ -0,0 +1,22 @@
+#-------------------------------------------------------------------------------
+# Name:        favorite_fruits
+# Purpose:
+#
+# Author:      Nag
+#
+# Created:     18.10.2018
+# Copyright:   (c) Nag 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+favorite_fruits = ['apple','banan','pineapple','avocado','persik']
+if 'grusha' in favorite_fruits:
+    print("I really like a grusha!")
+if 'banan' in favorite_fruits:
+    print("I really like a banan!")
+if 'pineapple' in favorite_fruits:
+    print("I really like a pineapple!")
+if 'avocado' in favorite_fruits:
+    print("I really like a avocado!")
+if 'persik' in favorite_fruits:
+    print("I really like a persik!")
