d78742e5e2dd979343418f94bdc520ce0bde0a60
Nikolay Goltsev <nag78@yandex.ru>
2018-10-22 19:11:39 +0300

    Словари

---
+++ favorite_laguages.py
@@ -0,0 +1,20 @@
+#-------------------------------------------------------------------------------
+# Name:        favorite_laguages
+# Purpose:
+#
+# Author:      Nag
+#
+# Created:     22.10.2018
+# Copyright:   (c) Nag 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+favorite_languages = {
+    'jen':'phyton',
+    'sarah':'c',
+    'edward':'ruby',
+    'phil':'python'
+    }
+print("Sarah's favorite language is " +
+    favorite_languages['sarah'].title() +
+    ".")
