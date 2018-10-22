d603162ffcab5a146a9023e2e9fa0b3bb11d0002
Nikolay Goltsev <nag78@yandex.ru>
2018-10-22 13:55:28 +0300

    Словари

--- alien.py
+++ alien.py
@@ -27,3 +27,15 @@ print ("The alien is " + alien_0['color'] + ".")
 
 alien_0['color'] = 'yellow'
 print ("The alien is now " + alien_0['color'] + ".")
+
+alien_0 = {'x_position':0,'y_position':25,'speed':'fast'}
+print("Original x-position: " + str(alien_0['x_position']))
+
+if alien_0['speed'] == 'slow':
+    x_increment = 1
+elif alien_0['speed'] == 'medium':
+    x_increment = 2
+else:
+    x_increment =3
+alien_0['x_position'] = alien_0['x_position'] + x_increment
+print("New x-position: " + str(alien_0['x_position']))
