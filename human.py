5e7917794080a4967d52984b13fdca7d0f05dc86
Nikolay Goltsev <nag78@yandex.ru>
2018-10-23 13:45:23 +0300

    Словари

---
+++ human.py
@@ -0,0 +1,19 @@
+#-------------------------------------------------------------------------------
+# Name:        human
+# Purpose:
+#
+# Author:      Nag
+#
+# Created:     23.10.2018
+# Copyright:   (c) Nag 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+human = {
+    'first_name' : 'nikolay',
+    'last_name' : 'goltsev',
+    'age' : 40,
+    'city' : 'moscow'
+    }
+print ("The is " + human['first_name'].title() + " " + human['last_name'].title() + ".\n")
+print ("He is live in city " + human['city'].title() + " " + str(human['age']) + " year's")
