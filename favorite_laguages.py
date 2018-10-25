3c323d316163dc0470bf061c8f5cefdae4664996
Nikolay Goltsev <nag78@yandex.ru>
2018-10-25 15:21:22 +0300

    Список в словаре

--- favorite_laguages.py
+++ favorite_laguages.py
@@ -10,19 +10,20 @@
 #-------------------------------------------------------------------------------
 
 favorite_languages = {
-    'jen':'python',
-    'sarah':'c',
-    'edward':'ruby',
-    'phil':'python'
+    'jen':['python', 'ruby'],
+    'sarah':['c'],
+    'edward':['ruby','go'],
+    'phil':['python','haskell'],
     }
 ##print("Sarah's favorite language is " +
 ##    favorite_languages['sarah'].title() +
 ##    ".\n")
 
 
-for name, language in favorite_languages.items():
-    print(name.title() + "'s favorite language is "+
-        language.title() + ".")
+for name, languages in favorite_languages.items():
+    print(name.title() + "'s favorite languages are:")
+    for language in languages:
+        print("\t" + language.title())
 
 print("\n")
 
@@ -30,21 +31,21 @@ print("\n")
 ##for name in favorite_languages.keys():
 ##    print(name.title())
 
-friends = ['phil','sarah']
-for name in sorted(favorite_languages.keys()):
-    print(name.title() + ", thank you for taking the poll.")
-
-    if name in friends:
-        print(" Hi " + name.title() +
-            ", I see your favorite language is " +
-            favorite_languages[name].title() + "!")
-
-if 'erin' not in favorite_languages.keys():
-    print("Erin, please take our poll!\n")
-
-print("The following languages have been mentioned:")
-for language in set(favorite_languages.values()):
-    print(language.title())
+##friends = ['phil','sarah']
+##for name in sorted(favorite_languages.keys()):
+##    print(name.title() + ", thank you for taking the poll.")
+##
+##    if name in friends:
+##        print(" Hi " + name.title() +
+##            ", I see your favorite language is " +
+##            favorite_languages[name].title() + "!")
+##
+##if 'erin' not in favorite_languages.keys():
+##    print("Erin, please take our poll!\n")
+##
+##print("The following languages have been mentioned:")
+##for language in set(favorite_languages.values()):
+##    print(language.title())
 
 
 
