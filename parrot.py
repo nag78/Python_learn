7bbc25893fe8ffaebf749f7e28d84ee838664b24
Nikolay Goltsev <nag78@yandex.ru>
2018-10-26 15:37:42 +0300

    Ввод данных

---
+++ parrot.py
@@ -0,0 +1,13 @@
+#-------------------------------------------------------------------------------
+# Name:        parrot
+# Purpose:
+#
+# Author:      Nag
+#
+# Created:     26.10.2018
+# Copyright:   (c) Nag 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+message = input("Tell me something, and I will repeat it back to you: ")
+print(message)
