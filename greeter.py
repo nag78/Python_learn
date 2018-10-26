2f5a47cae8afcdbde927e848b1baf82cd758676e
Nikolay Goltsev <nag78@yandex.ru>
2018-10-26 15:40:33 +0300

    Ввод

---
+++ greeter.py
@@ -0,0 +1,13 @@
+#-------------------------------------------------------------------------------
+# Name:        greeter
+# Purpose:
+#
+# Author:      Nag
+#
+# Created:     26.10.2018
+# Copyright:   (c) Nag 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+name = input("Please enter your name: ")
+print("Hello, " + name + "!")
