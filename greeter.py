d98780f7b4c53fa39afee00a2290f15db0b75329
Nikolay Goltsev <nag78@yandex.ru>
2018-10-26 15:44:51 +0300

    Ввод данных

--- greeter.py
+++ greeter.py
@@ -9,5 +9,12 @@
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
 
-name = input("Please enter your name: ")
-print("Hello, " + name + "!")
+##name = input("Please enter your name: ")
+##print("Hello, " + name + "!")
+
+prompt = "If you tell us who you are, we can personalize the messages you see."
+prompt += "\nWhat is your first name?"
+
+name = input(prompt)
+
+print("\nHello, " + name + "!")
