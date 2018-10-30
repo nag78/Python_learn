5dc6f947537f49e640c9dff4a4c50e3929e93e1b
Nikolay Goltsev <nag78@yandex.ru>
2018-10-30 15:07:07 +0300

    Цикл while

---
+++ counting.py
@@ -0,0 +1,16 @@
+#-------------------------------------------------------------------------------
+# Name:        counting
+# Purpose:
+#
+# Author:      Nag
+#
+# Created:     30.10.2018
+# Copyright:   (c) Nag 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+
+current_number = 1
+while current_number <= 5:
+    print(current_number)
+    current_number += 1
