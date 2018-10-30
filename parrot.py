66b0d9ab6b3cef6e7443fe598e858be5487b7b45
Nikolay Goltsev <nag78@yandex.ru>
2018-10-30 15:18:05 +0300

    флаг

--- parrot.py
+++ parrot.py
@@ -11,9 +11,20 @@
 
 prompt = "\nTell me something, and I will repeat it back to you: "
 prompt += "\nEnter 'quit' to end program. "
-message = ""
-while message != 'quit':
+
+active = True
+while active:
     message = input(prompt)
 
-    if message != 'quit':
+    if message == 'quit':
+        active = False
+    else:
         print(message)
+
+
+##message = ""
+##while message != 'quit':
+##    message = input(prompt)
+##
+##    if message != 'quit':
+##        print(message)
