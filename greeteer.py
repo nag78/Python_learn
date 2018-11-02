eefc09fa229676b52839a30a604a949fcd915f22
Nikolay Goltsev <nag78@yandex.ru>
2018-11-02 14:35:48 +0300

    функции

---
+++ greeteer.py
@@ -0,0 +1,16 @@
+#-------------------------------------------------------------------------------
+# Name:        greet_user
+# Purpose:
+#
+# Author:      Nag
+#
+# Created:     02.11.2018
+# Copyright:   (c) Nag 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def greet_user(username):
+    """Выводит простое приветствие"""
+    print ("Hello, " + username.title() + "!")
+greet_user('jesse')
+
