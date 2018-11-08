d3a508795d3cca799d749dfb45a17f9ed7a20135
Nikolay Goltsev <nag78@yandex.ru>
2018-11-08 15:57:03 +0300

    функции

---
+++ display_message.py
@@ -0,0 +1,17 @@
+#-------------------------------------------------------------------------------
+# Name:        display_message
+# Purpose:
+#
+# Author:      Nag
+#
+# Created:     08.11.2018
+# Copyright:   (c) Nag 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def display_message(message):
+    print (message)
+
+
+message=input("Message: ")
+display_message(message)
