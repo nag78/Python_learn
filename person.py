d920b89943e549a4aef0b5de9bcf64023827c200
Nikolay Goltsev <nag78@yandex.ru>
2018-11-14 17:46:08 +0300

    возврат словаря

---
+++ person.py
@@ -0,0 +1,20 @@
+#-------------------------------------------------------------------------------
+# Name:        person
+# Purpose:
+#
+# Author:      Nag
+#
+# Created:     14.11.2018
+# Copyright:   (c) Nag 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def build_person(first_name,last_name,age=''):
+    """Возвращает словарь с информацией о человеке"""
+    person = {'first':first_name, 'last':last_name}
+    if age:
+        person['age'] = age
+    return person
+
+musician = build_person('jimi','hendrix',age = 27)
+print(musician)
