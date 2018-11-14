327536db4c06c0ad6eb06374a0303fd4a0ab9f6d
Nikolay Goltsev <nag78@yandex.ru>
2018-11-14 17:37:13 +0300

    возвращаемое значение

--- formated_name.py
+++ formated_name.py
@@ -9,9 +9,16 @@
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
 
-def get_formated_name(first_name,last_name):
+def get_formated_name(first_name,last_name,middle_name =''):
     """Возвращает аккуратно отформатированное полное имя"""
-    full_name = first_name + ' ' + last_name
+    if middle_name:
+        full_name = first_name + ' ' + middle_name + ' ' + last_name
+    else:
+        full_name = first_name + ' '  + last_name
     return full_name.title()
+
 musician = get_formated_name('jimi','hendrix')
+print(musician)
+
+musician = get_formated_name('john','hooker','lee')
 print(musician)
