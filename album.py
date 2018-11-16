5d37dee28d05dbd00eb203f0fed5635a89aba3ea
Nikolay Goltsev <nag78@yandex.ru>
2018-11-16 12:21:52 +0300

    функции и цикл

--- album.py
+++ album.py
@@ -14,9 +14,24 @@ def make_album(musician,name_album,track=None):
     if track:
         album['track'] = track
     return album
-lube = make_album('lube','lube')
-bilan = make_album('bilan','nocnoi huligan')
-allegrova = make_album('allegrova','imperatrica',12)
-print(lube)
-print(bilan)
-print(allegrova)
+while True:
+    print("\nPlease tell me album:")
+    print("(please 'q' at any time to quit)")
+
+    m_name = input("Name musician: ")
+    if m_name == 'q':
+        break
+    m_album = input("Album: ")
+    if m_album == 'q':
+        break
+    m_track = input("No Track: ")
+    if m_track == 'q':
+        break
+    n_album = make_album(m_name,m_album,m_track)
+    print(n_album)
+##lube = make_album('lube','lube')
+##bilan = make_album('bilan','nocnoi huligan')
+##allegrova = make_album('allegrova','imperatrica',12)
+##print(lube)
+##print(bilan)
+##print(allegrova)
