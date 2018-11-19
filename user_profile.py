d6e0c3d79b846425cb7d69134e121d39b71825c3
Nikolay Goltsev <nag78@yandex.ru>
2018-11-19 16:34:04 +0300

    функции

--- user_profile.py
+++ user_profile.py
@@ -17,7 +17,7 @@ def build_profile(first,last,**user_info):
     for key, value in user_info.items():
         profile[key] = value
     return profile
-user_profile = build_profile('albert', 'einshtain',
-                                location = 'princeton',
-                                field = 'physics')
+user_profile = build_profile('nikola', 'goltsev',
+                                city = 'moscow',
+                                field = 'it')
 print(user_profile)
