afeadfa06513887dc27ca473e68ca71dbaa52681
Nikolay Goltsev <nag78@yandex.ru>
2018-11-19 16:13:46 +0300

    произвольный набор именованных аргументов

---
+++ user_profile.py
@@ -0,0 +1,23 @@
+#-------------------------------------------------------------------------------
+# Name:        user_profile
+# Purpose:
+#
+# Author:      Nag
+#
+# Created:     19.11.2018
+# Copyright:   (c) Nag 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def build_profile(first,last,**user_info):
+    """Строит словарь с информацией о пользователе."""
+    profile = {}
+    profile['first_name'] = first
+    profile['last_name'] = last
+    for key, value in user_info.items():
+        profile[key] = value
+    return profile
+user_profile = build_profile('albert', 'einshtain',
+                                location = 'princeton',
+                                field = 'physics')
+print(user_profile)
