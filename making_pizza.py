0106726611c06e0f82ce40f74872f198885f5483
Nikolay Goltsev <nag78@yandex.ru>
2018-11-20 15:39:30 +0300

    Update making_pizza.py

--- making_pizza.py
+++ making_pizza.py
@@ -9,6 +9,7 @@
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
 
-import pizza
-pizza.make_pizza(16,'pepperoni')
-pizza.make_pizza(12,'mushrooms','green peppers','extra cheese')
+from pizza import make_pizza as mp
+
+mp(16,'pepperoni')
+mp(12,'mushrooms','green peppers','extra cheese')
