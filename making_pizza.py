8bed688c0cae985b2c5175747f7049dfbf39614f
Nikolay Goltsev <nag78@yandex.ru>
2018-11-20 15:37:13 +0300

    Использование модулей

---
+++ making_pizza.py
@@ -0,0 +1,14 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Nag
+#
+# Created:     20.11.2018
+# Copyright:   (c) Nag 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+import pizza
+pizza.make_pizza(16,'pepperoni')
+pizza.make_pizza(12,'mushrooms','green peppers','extra cheese')
--- pizza.py
+++ pizza.py
@@ -1,61 +1,3 @@
-##pizzas = ['papa johns','margarita','pepperoni']
-##
-##friend_pizzas = pizzas[:]
-##pizzas.append('mexican')
-##friend_pizzas.append('gavai')
-##
-##print("My favorite pizzas are:")
-##print(pizzas)
-##
-##print("My friend's favorite pizzas are:")
-##print(friend_pizzas)
-##
-##
-##for pizza in pizzas:
-##    print("I like a " + pizza.title() + "!\n")
-##print ("I really love pizza!")
-##
-##for pizza in friend_pizzas:
-##    print("I like a " + pizza.title() + "!\n")
-##print ("I really love pizza!")
-##
-###Сохранение информации о заказанной пицце
-##
-##pizza = {
-##    'crust':'thick',
-##    'toppings':['mushrooms','extra cheese'],
-##    }
-##
-###Описание заказа.
-##
-##print("You ordered a " + pizza['crust'] + "-crustpizza " +
-##    "with the following toppings:")
-##for topping in pizza['toppings']:
-##    print("\t" + topping)
-
-##prompt = "\nEnter you want toping."
-##prompt += "\n(Enter 'quit' for exit!"
-
-##while True:
-##    topings = input(prompt)
-##    if topings == 'quit':
-##        break
-##    else:
-##        print("Plese add me in pizza " + topings.title() + "!")
-
-##active = True
-##while active:
-##    topings = input(prompt)
-##    if topings == 'quit':
-##        active = False
-##    else:
-##        print("Plese add me in pizza " + topings.title() + "!")
-##topings = ''
-##while topings != 'quit':
-##    topings = input(prompt)
-##    if topings != 'quit':
-##        print("Plese add me in pizza " + topings.title() + "!")
-
 def make_pizza(size,*toppings):
     """Вывод описания пиццы"""
     print("\nMaking a " + str(size) +
@@ -63,8 +5,6 @@ def make_pizza(size,*toppings):
     for topping in toppings:
         print("- " + topping)
 
-make_pizza(16,'pepperoni')
-make_pizza(12, 'mushrooms','green peppers','extra cheese')
 
 
 
