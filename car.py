01f3b36bf08f4b2ede1810f8eeaf9c95a1adc6a9
Nikolay Goltsev <nag78@yandex.ru>
2018-11-21 17:25:21 +0300

    Update car.py
    Наследование

--- car.py
+++ car.py
@@ -16,13 +16,13 @@ class Car():
         self.make = make
         self.model = model
         self.year = year
-#
         self.odometer_reading = 0
 
     def get_descriptive_name(self):
         """Возвращает аккуратно отформатировнное описание"""
         long_name = str(self.year) + ' ' + self.make + ' ' + self.model
         return long_name.title()
+
     def read_odometer(self):
         """Выводиит пробег машины в милях."""
         print("This car has " + str(self.odometer_reading) + " miles on it.")
@@ -33,9 +33,22 @@ class Car():
             self.odometer_reading = mileage
         else:
             print("You can't roll back an odometer!")
+
     def increment_odometer(self,miles):
         """Увеличивает показания одометра с заданным приращением"""
         self.odometer_reading += miles
+
+class ElectricCar(Car):
+    """Представляет аспект машины, специфические для электромобилей."""
+    def __init__(self, make, model, year):
+        """Инициализирует атрибуты класса-родителя."""
+        super().__init__(make, model, year)
+
+
+my_tesla = ElectricCar('tesla','model s',2016)
+print(my_tesla.get_descriptive_name())
+
+
 
 my_used_car = Car('subaru','outback',2013)
 print(my_used_car.get_descriptive_name())
