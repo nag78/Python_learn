0ae72de863b065d34dc10042cfdec5b6d8841d34
Nikolay Goltsev <nag78@yandex.ru>
2018-11-21 14:37:11 +0300

    Update car.py

--- car.py
+++ car.py
@@ -16,11 +16,16 @@ class Car():
         self.make = make
         self.model = model
         self.year = year
+        self.odometer_reading = 0
 
     def get_descriptive_name(self):
         """Возвращает аккуратно отформатировнное описание"""
         long_name = str(self.year) + ' ' + self.make + ' ' + self.model
         return long_name.title()
+    def read_odometer(self):
+        """Выводиит пробег машины в милях."""
+        print("This car has " + str(self.odometer_reading) + " miles on it.")
 
 my_new_car = Car('audi','a4',2016)
 print(my_new_car.get_descriptive_name())
+my_new_car.read_odometer()
