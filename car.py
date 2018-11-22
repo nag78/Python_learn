6c0a16ce3a92e0f478414a5fa3b8d1b853511e97
Nikolay Goltsev <nag78@yandex.ru>
2018-11-22 17:26:41 +0300

    Update car.py

--- car.py
+++ car.py
@@ -46,6 +46,16 @@ class Battery():
     def describe_battery(self):
         """Выводит информацию о мощности аккумулятора"""
         print("This car has a " + str(self.battery_size) + "kWh battery.")
+    def get_range(self):
+        """Выводит запас хода для аккумулятора"""
+        if self.battery_size == 70:
+            range = 240
+        elif self.battery_size == 85:
+            range = 270
+
+        message = "This car can go approximately " + str(range)
+        message += " miles on a full charge."
+        print(message)
 
 class ElectricCar(Car):
     """Представляет аспект машины, специфические для электромобилей."""
@@ -61,6 +71,7 @@ my_tesla = ElectricCar('tesla','model s',2016)
 print(my_tesla.get_descriptive_name())
 
 my_tesla.battery.describe_battery()
+my_tesla.battery.get_range()
 
 
 
