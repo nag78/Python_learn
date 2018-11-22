a174944dcdb619fa79823f98900c570f06ca5428
Nikolay Goltsev <nag78@yandex.ru>
2018-11-22 14:22:59 +0300

    Update car.py

--- car.py
+++ car.py
@@ -41,13 +41,20 @@ class Car():
 class ElectricCar(Car):
     """Представляет аспект машины, специфические для электромобилей."""
     def __init__(self, make, model, year):
-        """Инициализирует атрибуты класса-родителя."""
+        """Инициализирует атрибуты класса-родителя. Затем инициализируются
+        специфичные для электромобиля."""
         super().__init__(make, model, year)
+        self.battery_size = 70
+
+    def describe_battery(self):
+        """Выводит информацию о мощности аккумулятора"""
+        print("This car has a " + str(self.battery_size) + "kWh battery.")
 
 
 my_tesla = ElectricCar('tesla','model s',2016)
 print(my_tesla.get_descriptive_name())
 
+my_tesla.describe_battery()
 
 
 my_used_car = Car('subaru','outback',2013)
