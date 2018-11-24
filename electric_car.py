2b43657139c77ea7f0ac50396a665d05e6fc2597
nag78 <nag78@yandex.ru>
2018-11-24 19:48:55 +0300

    Update electric_car.py

--- electric_car.py
+++ electric_car.py
@@ -7,36 +7,7 @@
 # Created:     21.11.2018
 # Copyright:   (c) Nag 2018
 # Licence:     <your licence>
-#-------------------------------------------------------------------------------
-
-class Car():
-    """Простая модель автомобиля"""
-    def __init__(self,make,model,year):
-        """Инициализирует атрибуты описания автомобиля."""
-        self.make = make
-        self.model = model
-        self.year = year
-        self.odometer_reading = 0
-
-    def get_descriptive_name(self):
-        """Возвращает аккуратно отформатировнное описание"""
-        long_name = str(self.year) + ' ' + self.make + ' ' + self.model
-        return long_name.title()
-
-    def read_odometer(self):
-        """Выводиит пробег машины в милях."""
-        print("This car has " + str(self.odometer_reading) + " miles on it.")
-
-    def update_odometer(self, mileage):
-        """Устанавливает заданное значение на одометре."""
-        if mileage >= self.odometer_reading:
-            self.odometer_reading = mileage
-        else:
-            print("You can't roll back an odometer!")
-
-    def increment_odometer(self,miles):
-        """Увеличивает показания одометра с заданным приращением"""
-        self.odometer_reading += miles
+from car import Car
 
 class Battery():
     """Простая модель аккумулятора автомобиля"""
@@ -59,8 +30,6 @@ class Battery():
         if self.battery_size != 85:
             self.battery_size = 85
 
-
-
 class ElectricCar(Car):
     """Представляет аспект машины, специфические для электромобилей."""
     def __init__(self, make, model, year):
@@ -71,21 +40,3 @@ class ElectricCar(Car):
 
 
 
-my_tesla = ElectricCar('tesla','model s',2016)
-print(my_tesla.get_descriptive_name())
-
-my_tesla.battery.describe_battery()
-my_tesla.battery.get_range()
-my_tesla.battery.upgrade_battery()
-my_tesla.battery.get_range()
-
-
-
-my_used_car = Car('subaru','outback',2013)
-print(my_used_car.get_descriptive_name())
-
-my_used_car.update_odometer(23500)
-my_used_car.read_odometer()
-
-my_used_car.increment_odometer(100)
-my_used_car.read_odometer()
