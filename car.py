a9ae8e7f8ec7776fc3c84b749f9546c68053e72f
nag78 <nag78@yandex.ru>
2018-11-24 19:48:51 +0300

    Update car.py

--- car.py
+++ car.py
@@ -38,54 +38,7 @@ class Car():
         """Увеличивает показания одометра с заданным приращением"""
         self.odometer_reading += miles
 
-class Battery():
-    """Простая модель аккумулятора автомобиля"""
-    def __init__ (self, battery_size = 70):
-        """Инициализирует аттрибуты аккумулятора"""
-        self.battery_size = battery_size
-    def describe_battery(self):
-        """Выводит информацию о мощности аккумулятора"""
-        print("This car has a " + str(self.battery_size) + "kWh battery.")
-    def get_range(self):
-        """Выводит запас хода для аккумулятора"""
-        if self.battery_size == 70:
-            range = 240
-        elif self.battery_size == 85:
-            range = 270
-        message = "This car can go approximately " + str(range)
-        message += " miles on a full charge."
-        print(message)
-    def upgrade_battery(self):
-        if self.battery_size != 85:
-            self.battery_size = 85
 
 
 
-class ElectricCar(Car):
-    """Представляет аспект машины, специфические для электромобилей."""
-    def __init__(self, make, model, year):
-        """Инициализирует атрибуты класса-родителя. Затем инициализируются
-        специфичные для электромобиля."""
-        super().__init__(make, model, year)
-        self.battery = Battery()
 
-
-
-my_tesla = ElectricCar('tesla','model s',2016)
-print(my_tesla.get_descriptive_name())
-
-my_tesla.battery.describe_battery()
-my_tesla.battery.get_range()
-my_tesla.battery.upgrade_battery()
-my_tesla.battery.get_range()
-
-
-
-my_used_car = Car('subaru','outback',2013)
-print(my_used_car.get_descriptive_name())
-
-my_used_car.update_odometer(23500)
-my_used_car.read_odometer()
-
-my_used_car.increment_odometer(100)
-my_used_car.read_odometer()
