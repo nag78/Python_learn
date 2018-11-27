5ac7b5f0c938486cf0cea4bc1009d24aef06af2c
Nikolay Goltsev <nag78@yandex.ru>
2018-11-27 14:37:09 +0300

    рефакторинг со стандартной библиотекой

--- favorite_laguages.py
+++ favorite_laguages.py
@@ -8,50 +8,14 @@
 # Copyright:   (c) Nag 2018
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
+from collections import OrderedDict
 
-favorite_languages = {
-    'jen':['python', 'ruby'],
-    'sarah':['c'],
-    'edward':['ruby','go'],
-    'phil':['python','haskell'],
-    }
-##print("Sarah's favorite language is " +
-##    favorite_languages['sarah'].title() +
-##    ".\n")
+favorite_languages = OrderedDict()
 
-
-for name, languages in favorite_languages.items():
-    print(name.title() + "'s favorite languages are:")
-    for language in languages:
-        print("\t" + language.title())
-
-print("\n")
-
-
-##for name in favorite_languages.keys():
-##    print(name.title())
-
-##friends = ['phil','sarah']
-##for name in sorted(favorite_languages.keys()):
-##    print(name.title() + ", thank you for taking the poll.")
-##
-##    if name in friends:
-##        print(" Hi " + name.title() +
-##            ", I see your favorite language is " +
-##            favorite_languages[name].title() + "!")
-##
-##if 'erin' not in favorite_languages.keys():
-##    print("Erin, please take our poll!\n")
-##
-##print("The following languages have been mentioned:")
-##for language in set(favorite_languages.values()):
-##    print(language.title())
-
-
-
-pools = ['nikolay','sarah','svetlana','phil','marina','ilya']
-for name in pools:
-    if name in favorite_languages:
-        print(name.title() + ", спасибо за участие в опросе!")
-    else:
-        print(name.title() + ", пожалуйста примите участие в опросе!")
+favorite_languages['jen'] = 'python'
+favorite_languages['sarah'] = 'c'
+favorite_languages['edward'] = 'ruby'
+favorite_languages['phil'] = 'python'
+for name, language in favorite_languages.items():
+    print(name.title() + "'s favorite_language is " +
+    language.title() + ".")
