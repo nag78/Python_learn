91ee446d98e30e2888c8d30d19102b45165960a4
Nikolay Goltsev <nag78@yandex.ru>
2018-11-29 17:43:49 +0300

    json

--- remember_me.py
+++ remember_me.py
@@ -9,11 +9,15 @@
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
 import json
-
-username = input("What is your name? ")
-
 filename = 'username.json'
-with open(filename, 'w') as f_obj:
-    json.dump(username, f_obj)
-    print("We''ll remember you when you come back, " + username +
+try:
+    with open(filename) as f_obj:
+        username = json.load(f_obj)
+except FileNotFoundError:
+    username = input("What is your name? ")
+    with open(filename,'w') as f_obj:
+        json.dump(username, f_obj)
+        print("We''ll remember you when you come back, " + username +
         "!")
+else:
+    print("Welcome back, " + username + "!")
