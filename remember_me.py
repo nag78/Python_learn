ec25e98fbe5b6ac2689664b2591be9c292686ce0
Nikolay Goltsev <nag78@yandex.ru>
2018-11-30 17:35:03 +0300

    Update remember_me.py

--- remember_me.py
+++ remember_me.py
@@ -11,17 +11,22 @@ def get_stored_username():
     else:
         return username
 
+def get_new_username():
+    """Запрашивает хранимое имя пользоватля."""
+    username = input("What is your name? ")
+    filename = 'username.json'
+    with open(filename) as f_obj:
+        json.dump(username,f_obj)
+    return username
+
 def greet_user():
     """Приветствует пользователя по имени"""
     username = get_stored_username()
     if username:
         print("Welcome back, " + username + "!")
     else:
-        username = input("What is your name? ")
-        filename = 'username.json'
-        with open(filename) as f_obj:
-            json.dump(username,f_obj)
-            print("We'll remember you when you come back, " + username + "!")
+        username = get_new_username()
+        print("We'll remember you when you come back, " + username + "!")
 
 
 greet_user()
