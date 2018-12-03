ac410e7259c6626f4c34038a6411d1a8608e563f
Nikolay Goltsev <nag78@yandex.ru>
2018-12-03 19:02:09 +0300

    Update test_survey.py

--- test_survey.py
+++ test_survey.py
@@ -11,4 +11,18 @@ class TestAnonymousSurvey(unittest.TestCase):
         my_survey.store_response('English')
 
         self.assertIn('English',my_survey.responses)
+
+    def test_store_three_response(self):
+        """Проверяет, что три ответа были сохранены правильно."""
+        question = "What language did you first learn to speak?"
+        my_survey = AnonymousSurvey(question)
+        responses = ['English','Spanish','Mandarin']
+        for response in responses:
+            my_survey.store_response(response)
+
+        for response in responses:
+            self.assertIn(response,my_survey.responses)
+
+
+
 unittest.main()
