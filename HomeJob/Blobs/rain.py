7ffd283b3339503c269540cdd58bdf7e854e96a3
nag78 <nag78@yandex.ru>
2019-01-04 19:05:00 +0300

    Up

--- HomeJob/Blobs/rain.py
+++ HomeJob/Blobs/rain.py
@@ -1,11 +1,15 @@
 import sys
 import pygame
+from settings import Settings
 
 
 def run_game():
-    # Инициализирует игру и создает объект
+    # Инициализирует pygame, settings и создает объект ээкрана
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    settings = Settings()
+    screen = pygame.display.set_mode((
+                                      settings.screen_width,
+                                      settings.screen_height))
     pygame.display.set_caption("Дождь")
 
     # Запуск основного цикла игры.
@@ -14,7 +18,8 @@ def run_game():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-
+        # При каждом проходе перерисовывается экран
+        screen.fill(settings.bg_color)
         # Отображение последнего прорисованного экрана
         pygame.display.flip()
 
---
+++ HomeJob/Blobs/settings.py
@@ -0,0 +1,8 @@
+class Settings():
+    """Класс для хранения всех настроек"""
+    def __init__(self):
+        """Инициализируем настройки игры"""
+        # Параметры экрана
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (0, 100, 230)
