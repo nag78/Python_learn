01ed165ab98bbdd75ab75132bc1b7b8b766d3cfc
Nikolay Goltsev <nag78@yandex.ru>
2019-01-15 15:34:05 +0300

    Up

---
+++ python_work/scatter_squares.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+
+x_values = [1, 2, 3, 4 ,5]
+y_values = [1, 4, 9, 16, 25]
+plt.scatter(x_values, y_values, s=20)
+
+# Назначение заголовка диаграммы и осей меток.
+plt.title("Square Numbers", fontsize = 24)
+plt.xlabel("Value", fontsize = 14)
+plt.ylabel("Square of Value", fontsize = 14)
+
+# Назначение размера шрифта делений на осях.
+plt.tick_params(axis = 'both', which = 'major', labelsize = 14)
+
+plt.show()
