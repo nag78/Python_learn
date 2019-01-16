15cc56003ee28d07f0687fc04639a99daa7d74f3
Nikolay Goltsev <nag78@yandex.ru>
2019-01-16 15:49:32 +0300

    Update

--- python_work/random_walk.py
+++ python_work/random_walk.py
@@ -4,7 +4,7 @@ from random import choice
 class RandomWalk():
     """Класс для генерирования случайных блужданий."""
 
-    def __init__(self, num_points = 5000):
+    def __init__(self, num_points=5000):
         """Инициализирует атрибуты блуждания."""
         self.num_points = num_points
 
@@ -12,25 +12,26 @@ class RandomWalk():
         self.x_values = [0]
         self.y_values = [0]
 
+    def get_step(self):
+        """Вычисляем точки блуждания."""
+        # Определение направления и длины перемещения.
+        direction = choice([1, -1])
+        distance = choice([0, 1, 2, 3, 4])
+        step = direction * distance
+        return (step)
 
     def fill_walk(self):
         """Вычисляет все точки блуждания."""
 
         # Шаги генерируются до достижения нужной длины.
         while len(self.x_values) < self.num_points:
-            # Определение направления и длины перемещения.
-            x_direction = choice([1, -1])
-            x_distance = choice([0, 1, 2, 3, 4])
-            x_step = x_direction * x_distance
-
-            y_direction = choice([1, -1])
-            y_distance = choice([0, 1, 2, 3, 4])
-            y_step = y_direction * y_distance
+            x_step = self.get_step()
+            y_step = self.get_step()
 
             # Отклонение нулевых перемещений.
             if x_step == 0 and y_step == 0:
                 continue
-            
+
             # Вычисление следующих значений x и y.
             next_x = self.x_values[-1] + x_step
             next_y = self.y_values[-1] + y_step
--- python_work/rw_visual.py
+++ python_work/rw_visual.py
@@ -6,13 +6,19 @@ from random_walk import RandomWalk
 while True:
     rw = RandomWalk()
     rw.fill_walk()
+
+    # Назначение размера области экрана.
+    plt.figure(figsize=(10, 6))
     point_numbers = list(range(rw.num_points))
-    plt.scatter(rw.x_values, rw.y_values, s=15, c=point_numbers,
-                cmap=plt.cm.Blues, edgecolors='none')
+    plt.plot(rw.x_values, rw.y_values, linewidth=1)
     # Выделение первой и последней точек.
-    plt.scatter(0, 0, c='green', edgecolors='none', s=15)
+    plt.scatter(0, 0, c='green', edgecolors='none', s=100)
     plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none',
-                s=15)
+                s=100)
+    # Удаление осей.
+    plt.axes().get_xaxis().set_visible(False)
+    plt.axes().get_yaxis().set_visible(False)
+
     plt.show()
     keep_running = input("Make another walk? (y/n): ")
     if keep_running == 'n':
