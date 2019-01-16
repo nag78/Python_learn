964665bf7b204ba3fea5d8f8c889390f319c251f
Nikolay Goltsev <nag78@yandex.ru>
2019-01-16 14:33:52 +0300

    Up

--- python_work/rw_visual.py
+++ python_work/rw_visual.py
@@ -5,13 +5,14 @@ from random_walk import RandomWalk
 # Новые блуждания строятся до тех пор, пока программа остается активной.
 while True:
     rw = RandomWalk()
-    rw.fill_walk()  
+    rw.fill_walk()
     point_numbers = list(range(rw.num_points))
-    plt.scatter(rw.x_values, rw.y_values, s = 15, c = point_numbers, cmap = plt.cm.Blues,
-                edgecolors='none')
+    plt.scatter(rw.x_values, rw.y_values, s=15, c=point_numbers,
+                cmap=plt.cm.Blues, edgecolors='none')
     # Выделение первой и последней точек.
-    plt.scatter(0, 0, c = 'green', edgecolors='none', s=100)
-    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+    plt.scatter(0, 0, c='green', edgecolors='none', s=15)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none',
+                s=15)
     plt.show()
     keep_running = input("Make another walk? (y/n): ")
     if keep_running == 'n':
