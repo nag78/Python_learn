3f1b521185776fde992ec4de114bf0ba5bf67794
Nikolay Goltsev <nag78@yandex.ru>
2019-01-16 14:31:17 +0300

    Update rw_visual.py

--- python_work/rw_visual.py
+++ python_work/rw_visual.py
@@ -6,7 +6,12 @@ from random_walk import RandomWalk
 while True:
     rw = RandomWalk()
     rw.fill_walk()  
-    plt.scatter(rw.x_values, rw.y_values, s = 15, c = rw.y_values)
+    point_numbers = list(range(rw.num_points))
+    plt.scatter(rw.x_values, rw.y_values, s = 15, c = point_numbers, cmap = plt.cm.Blues,
+                edgecolors='none')
+    # Выделение первой и последней точек.
+    plt.scatter(0, 0, c = 'green', edgecolors='none', s=100)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
     plt.show()
     keep_running = input("Make another walk? (y/n): ")
     if keep_running == 'n':
