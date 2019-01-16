169b29739169439d33993e1ab18b9d9ffa894f9a
Nikolay Goltsev <nag78@yandex.ru>
2019-01-16 14:17:26 +0300

    Update rw_visual.py

--- python_work/rw_visual.py
+++ python_work/rw_visual.py
@@ -2,8 +2,12 @@ import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
 
-# Построение случайного блуждания и нанесение точек на диаграмму.
-rw = RandomWalk()
-rw.fill_walk()
-plt.scatter(rw.x_values, rw.y_values, s = 15, c = rw.y_values)
-plt.show()
+# Новые блуждания строятся до тех пор, пока программа остается активной.
+while True:
+    rw = RandomWalk()
+    rw.fill_walk()  
+    plt.scatter(rw.x_values, rw.y_values, s = 15, c = rw.y_values)
+    plt.show()
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        break
