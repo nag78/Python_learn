b1bc0f04740b1161689c7c2d58e9f65ab4623f1a
Nikolay Goltsev <nag78@yandex.ru>
2019-01-21 17:13:20 +0300

    Update high_lows.py

--- python_work/high_lows.py
+++ python_work/high_lows.py
@@ -4,19 +4,23 @@ from datetime import datetime
 
 
 # Чтение дат, температурных максимумов и минимумов из файла
-filename = ".\\csv\\sitka_weather_2014.csv"
+filename = ".\\csv\\death_valley_2014.csv"
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
     dates, highs, lows = [], [], []
     for row in reader:
-        current_date = datetime.strptime(row[0], "%Y-%m-%d")
-        dates.append(current_date)
-        high = int(row[1])
-        highs.append(high)
-        low = int(row[3])
-        lows.append(low)
-    print(highs)
+        try:
+            current_date = datetime.strptime(row[0], "%Y-%m-%d")
+            low = int(row[3])
+            high = int(row[1])
+        except ValueError:
+            print(current_date, 'missing data')
+        else:
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
+
 # Нанесение данных на диаграмму.
 fig = plt.figure(dpi=128, figsize=(10, 6))
 plt.plot(dates, highs, c='red', alpha=0.5)
@@ -24,7 +28,8 @@ plt.plot(dates, lows, c='blue', alpha=0.5)
 plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 # Форматирование диаграммы.
-plt.title("Daily high and low temperatures - 2014", fontsize=24)
+title = "Daily high and low temperatures - 2014\nDeath Valley, CA"
+plt.title(title, fontsize=20)
 plt.xlabel('', fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=16)
