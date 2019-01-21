f98c04305c83d4d15268782bf1381e790c4750dd
Nikolay Goltsev <nag78@yandex.ru>
2019-01-21 15:28:10 +0300

    Update high_lows.py

--- python_work/high_lows.py
+++ python_work/high_lows.py
@@ -1,24 +1,28 @@
 import csv
 from matplotlib import pyplot as plt
+from datetime import datetime
 
 
-# Чтение максимальных температур из файла
+# Чтение дат и максимальных температур из файла
 filename = ".\\csv\\sitka_weather_07-2014.csv"
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
-    highs = []
+    dates, highs = [], []
     for row in reader:
+        current_date = datetime.strptime(row[0], "%Y-%m-%d")
+        dates.append(current_date)
         high = int(row[1])
         highs.append(high)
     print(highs)
 # Нанесение данных на диаграмму.
 fig = plt.figure(dpi=128, figsize=(10, 6))
-plt.plot(highs, c='red')
+plt.plot(dates, highs, c='red')
 
 # Форматирование диаграммы.
 plt.title("Daily high temperatures, July 2014", fontsize=24)
 plt.xlabel('', fontsize=16)
+fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=16)
 plt.tick_params(axis='both', which='major', labelsize=16)
 
