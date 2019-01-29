c6d13dbe27714da15761bcf66747f26399f5d3e2
Nikolay Goltsev <nag78@yandex.ru>
2019-01-29 16:33:47 +0300

    Update world_population.py

--- python_work/world_population.py
+++ python_work/world_population.py
@@ -17,7 +17,21 @@ for pop_dict in pop_data:
         code = get_country_code(country)
         if code:
             cc_populations[code] = population
+            # Группировка стран по уровню населения
+            cc_pops_1, cc_pops_2, cc_pops_3 = {}, {}, {}
+            for cc, pop in cc_populations.items():
+                if pop < 10000000:
+                    cc_pops_1[cc] = pop
+                elif pop < 1000000000:
+                    cc_pops_2[cc] = pop
+                else:
+                    cc_pops_3[cc] = pop
+            # Проверка количества стран на каждом уровне
+            print(len(cc_pops_1), len(cc_pops_2), len(cc_pops_3))
+
             wm = pygal.maps.world.World()
             wm.title = 'World Population in 2010, by Country'
-            wm.add('2010', cc_populations)
+            wm.add('0-10m', cc_pops_1)
+            wm.add('10m-1bn', cc_pops_2)
+            wm.add('>1bn', cc_pops_3)
 wm.render_in_browser()
