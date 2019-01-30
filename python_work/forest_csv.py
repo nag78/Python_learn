13212a051575fb996e0db48e3e36e5337246b652
Nikolay Goltsev <nag78@yandex.ru>
2019-01-30 19:39:29 +0300

    Up

---
+++ python_work/forest_csv.py
@@ -0,0 +1,33 @@
+import csv
+from country_code import get_country_code
+import pygal
+from pygal.style import LightGreenStyle as ST
+
+
+# Чтение файла
+fname = ".\\csv\\forests_area.csv"
+# y = '2018'
+# prompts = 'Выведите год от 1960 до 2018 включительно'
+# row_count = [range(1960, 2018)]
+forestes = {}
+with open(fname) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+
+    for row in reader:
+        country = row[0]
+        try:
+            forest = int(float(row[60]))
+        except ValueError:
+            print("error data")
+        else:
+            code = get_country_code(country)
+            if code:
+                forestes[code] = forest
+print(forestes)
+wm_style = ST()
+wm = pygal.maps.world.World(style=wm_style)
+wm.title = 'Forest area (% of land area) in 2018, by Country'
+wm.add('2018', forestes)
+
+wm.render_in_browser()
