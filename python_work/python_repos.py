7a0f1c6d34427c27b7b48e00a061214d07a3b91e
Nikolay Goltsev <nag78@yandex.ru>
2019-01-31 15:46:47 +0300

    Update python_repos.py

--- python_work/python_repos.py
+++ python_work/python_repos.py
@@ -8,6 +8,14 @@ print("Status code:", r.status_code)
 
 # Сохранение ответа API в переменной.
 response_dict = r.json()
+print("Total repositories:", response_dict['total_count'])
 
-# Обработка результатов
-print(response_dict.keys())
+# Анализ информации об репозиториях
+repo_dicts = response_dict['items']
+print("Repositories returned:", len(repo_dicts))
+
+# Анализ первого репозитория
+repo_dict = repo_dicts[0]
+print("\nKeys:", len(repo_dict))
+for key in sorted(repo_dict.keys()):
+    print(key)
