78b124cceb08a3d7e54c49aca38f1a4e9e0f5219
Nikolay Goltsev <nag78@yandex.ru>
2019-01-31 18:34:11 +0300

    Update python_repos.py

--- python_work/python_repos.py
+++ python_work/python_repos.py
@@ -1,4 +1,6 @@
 import requests
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
 
 
 # Создание вызова API и сохранение ответа
@@ -12,15 +14,17 @@ print("Total repositories:", response_dict['total_count'])
 
 # Анализ информации об репозиториях
 repo_dicts = response_dict['items']
-print("Repositories returned:", len(repo_dicts))
 
-# Анализ информации о репозиториях
+names, stars = [], []
 for repo_dict in repo_dicts:
-    print("\nSelected information about each repository:")
-    print('Name: ', repo_dict['name'])
-    print('Owner: ', repo_dict['owner']['login'])
-    print('Stars: ', repo_dict['stargazers_count'])
-    print('Repository: ', repo_dict['html_url'])
-    print('Created: ', repo_dict['created_at'])
-    print('Update: ', repo_dict['updated_at'])
-    print('Description: ', repo_dict['description'])
+    names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+
+# Построение визуализации
+my_style = LS('#333366', base_style=LCS)
+chart = pygal.Bar(style=my_style, x_label_rotation=45, show_legend=False)
+chart.title = 'Most-Starred Python Projects on GitHub'
+chart.x_labels = names
+
+chart.add('', stars)
+chart.render_in_browser()
