861bb55f26b15cf11a6ea6b13d859c4aeb495350
Nikolay Goltsev <nag78@yandex.ru>
2019-01-31 15:35:17 +0300

    Create python_repos.py

---
+++ python_work/python_repos.py
@@ -0,0 +1,13 @@
+import requests
+
+
+# Создание вызова API и сохранение ответа
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+r = requests.get(url)
+print("Status code:", r.status_code)
+
+# Сохранение ответа API в переменной.
+response_dict = r.json()
+
+# Обработка результатов
+print(response_dict.keys())
