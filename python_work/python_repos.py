3b72cabb0b4c0ddf67d3d1e1dfd634005309bc10
Nikolay Goltsev <nag78@yandex.ru>
2019-02-01 15:47:00 +0300

    Update python_repos.py

--- python_work/python_repos.py
+++ python_work/python_repos.py
@@ -15,10 +15,14 @@ print("Total repositories:", response_dict['total_count'])
 # Анализ информации об репозиториях
 repo_dicts = response_dict['items']
 
-names, stars = [], []
+names, plot_dicts = [], []
 for repo_dict in repo_dicts:
     names.append(repo_dict['name'])
-    stars.append(repo_dict['stargazers_count'])
+    plot_dict = {
+                 'value': repo_dict['stargazers_count'],
+                 'label': repo_dict['description'],
+                }
+    plot_dicts.append(plot_dict)
 
 # Построение визуализации
 my_style = LS('#333366', base_style=LCS)
@@ -35,5 +39,5 @@ chart = pygal.Bar(my_config, style=my_style)
 chart.title = 'Most-Starred Python Projects on GitHub'
 chart.x_labels = names
 
-chart.add('', stars)
-chart.render_in_browser()
+chart.add('', plot_dicts)
+chart.render_in_browser().decode('utf-8')
