ccf839bca3d00608268d6f1b5e7f4517b79030fd
Nikolay Goltsev <nag78@yandex.ru>
2019-02-11 17:29:24 +0300

    Up

--- learning_log/learning_logs/models.py
+++ learning_log/learning_logs/models.py
@@ -5,6 +5,20 @@ class Topic(models.Model):
     """Тема, которую изучает пользователь"""
     text = models.CharField(max_length= 200)
     date_add = models.DateTimeField(auto_now_add= True)
+
     def __str__(self):
         """Возвращает строковое представление модели."""
         return self.text
+
+class Entry(models.Model):
+    """Инфрмация изученная пользователем по теме"""
+    topic = models.ForeignKey(Topic, on_delete=models.PROTECT)
+    text = models.TextField()
+    date_add = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'enteries'
+
+    def __str__(self):
+        """Возвращает строковое представление модели."""
+        return str(self.text)[:50] + "..."
