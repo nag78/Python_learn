06c3c6c95919a8acd79a498033a444fedd2060ed
Nikolay Goltsev <nag78@yandex.ru>
2019-02-12 18:37:16 +0300

    Up

--- learning_log/learning_logs/models.py
+++ learning_log/learning_logs/models.py
@@ -17,7 +17,7 @@ class Entry(models.Model):
     date_add = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        verbose_name_plural = 'enteries'
+        verbose_name_plural = 'entries'
 
     def __str__(self):
         """Возвращает строковое представление модели."""
--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     # Вывод всех тем 
     url(r'topics/$', views.topics, name='topics'),
+    # Страница с подробной информацией по отдельной теме.
+    url(r'^topics/(?P<topic_id>\d+)/$', views.topics, name='topic'),
 ]
