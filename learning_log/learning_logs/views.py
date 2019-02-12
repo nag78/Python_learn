01b544ea8da34aa0ec8375c58e5c61796ba0fd39
Nikolay Goltsev <nag78@yandex.ru>
2019-02-12 15:05:27 +0300

    Up

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 
 def index(request):
     """Домашняя страница приложения Learning Log"""
-    return render(request, '/learning_logs/index.html')
+    return render(request, 'index.html')
