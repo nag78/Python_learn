1ff5f835c15ac5d4a6d3dac2cf9c4ed3a516900d
Nikolay Goltsev <nag78@yandex.ru>
2019-02-13 19:07:13 +0300

    Up

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     url(r'topics/$', views.topics, name='topics'),
     # Страница с подробной информацией по отдельной теме.
     url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
+    # Страница для добавления новой темы
+    url(r'^new_topic/$', views.new_topic, name='new_topic'),
 ]
--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Topic
+from .forms import TopicForm
 
 # Create your views here.
 
@@ -19,3 +20,18 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic' : topic, 'entries' : entries}
     return render(request, 'topic.html', context)
+
+def new_topic(request):
+    """Определяет новую тему"""
+    if request.method != 'POST':
+        # Данные не отправлялись; создается пустая форма.
+        form = TopicForm()
+    else:
+        # Отправлены данные POST; обработать данные
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs:topic'))
+
+    context = {'form': form}
+    return render(request, 'new_topic.html', context)
