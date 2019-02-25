c35b9520b360132328e2f43ab3e54dffb42b0a7b
Nikolay Goltsev <nag78@yandex.ru>
2019-02-25 19:34:39 +0300

    Up

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from .models import Topic, Entry
@@ -16,7 +16,7 @@ def index(request):
 @login_required
 def topics(request):
     """Выводит список тем"""
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     context = {'topics': topics}
     return render(request, 'topics.html', context)
 
@@ -25,6 +25,9 @@ def topics(request):
 def topic(request, topic_id):
     """Выводит одну тему и все ее записи."""
     topic = Topic.objects.get(id=topic_id)
+    # Проверка того, что тема принадлежит текущему пользователю.
+    if topic.owner != request.user:
+        raise Http404
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'topic.html', context)
@@ -73,6 +76,8 @@ def edit_entry(request, entry_id):
     """Редактирование существующей записи"""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+    if topic.owner != request.user:
+        raise Http404
 
     if request.method != 'POST':
         # Исходный запрос; форма заполняется данными текущей записи.
