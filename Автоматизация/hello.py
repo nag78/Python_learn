0cbee9b9e532e3e88c8df8c0d0244cb1b526486f
Nikolay Goltsev <nag78@yandex.ru>
2019-02-28 19:20:21 +0300

    Up

---
+++ Автоматизация/hello.py
@@ -0,0 +1,13 @@
+#%%
+# Программа приветствует пользователя и запрашивает
+# ввод информации.
+
+print('Hello world!')
+print('What is your name?') # запрос имени
+myName = input()
+print('It is good to meet you, ' + myName)
+print('The length of your name is:')
+print(len(myName))
+print('What is your age?') # запрос возраста
+myAge = input()
+print('you will be ' + str(int(myAge) + 1) + ' in a year.')
