1cd93e6b484c0b75f1c2dfa6ee803d3ed50f6da6
Nikolay Goltsev <nag78@yandex.ru>
2019-03-01 18:53:10 +0300

    Create collatz.py

---
+++ Автоматизация/collatz.py
@@ -0,0 +1,18 @@
+from random import randint as r
+
+
+
+
+def collatz(number):
+    """Последовательность Коллатца."""
+
+    if number % 2 == 0:
+        number = number // 2
+    else:
+        number = 3 * number + 1
+    print(str(number))
+    return number
+
+
+for guess in range(1, 100):
+    collatz(guess)
