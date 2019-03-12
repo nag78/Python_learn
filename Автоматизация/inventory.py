1ac259fdfa60cd4f7c533fcea481b69bcbcd37bd
Nikolay Goltsev <nag78@yandex.ru>
2019-03-12 19:38:59 +0300

    Update inventory.py

--- Автоматизация/inventory.py
+++ Автоматизация/inventory.py
@@ -1,4 +1,7 @@
-inventory = {'rope': 1, 'gold coin': 42, 'torch': 6, 'dagger': 1, 'arrow': 12}
+from collections import Counter
+
+stuff = {'rope': 1, 'gold coin': 42, 'torch': 6, 'dagger': 1, 'arrow': 12}
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
 
 def displayInventory(inventory):
     print('Inventory:')
@@ -8,6 +11,15 @@ def displayInventory(inventory):
         item_total += v
     print ('Total number of items: ' + str(item_total))
 
-displayInventory(inventory)
+def addToInventory(inventory, addItems):
+    loot = Counter(addItems)
+    for k in loot:
+        if k in inventory:
+            inventory[k] = inventory[k] + loot[k]
+        else:
+            inventory[k] = 1
+    return inventory
+c = addToInventory(stuff, dragonLoot)
+displayInventory(c)
 
 
