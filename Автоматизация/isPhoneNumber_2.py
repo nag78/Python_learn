5cbcf49aa2fa51afcac24c9142252839af2195c2
Nikolay Goltsev <nag78@yandex.ru>
2019-03-19 18:08:39 +0300

    Update isPhoneNumber_2.py

--- Автоматизация/isPhoneNumber_2.py
+++ Автоматизация/isPhoneNumber_2.py
@@ -3,9 +3,9 @@ from re import compile as re_cmpl
 
 message = '''
             Позвони мне завтра по номеру 415-555-1011.
-            415-555-9999 - это телефонный номер моего офиса.
+            415-555-9999, 320-8107 - это телефонный номер моего офиса.
           '''
-phoneNumRegx = re_cmpl(r'\d{3}-\d{3}-\d{4}')
+phoneNumRegx = re_cmpl(r'(\d{3,4}-)?(\d{3}-\d{4})')
 mo = phoneNumRegx.findall(message)
 
 for n in mo:
