c34a87c0c81ed372ec174d2deb484bc3f5fd3574
Nikolay Goltsev <nag78@yandex.ru>
2019-04-23 17:38:39 +0300

    Update MadLibs.py

--- Al Sweigart/MadLibs/MadLibs.py
+++ Al Sweigart/MadLibs/MadLibs.py
@@ -10,32 +10,33 @@ infile = open('d:\\Python\\Python_learn\\Al Sweigart\\MadLibs\\infile.txt',
 outfile = open('d:\\Python\\Python_learn\\Al Sweigart\\MadLibs\\outfile.txt',
                'w')
 inStr = infile.read()
+words = ['NOUN', 'ADJECTIVE', 'VERB', 'ADVERB', ]
 
 
 def findWord(inS, word):
     # Найти и заменить слова в строке.
     outS = ''
+# TODO: Переписать с использованием словоря.
 
-    if word == 'NOUN':
+    if word == words[0]:
         prompt = 'Введите существительное. '
         newWord = input(prompt)
         outS = inS.replace(word, newWord, 1)
-    elif word == 'ADJECTIVE':
+    elif word == words[1]:
         prompt = 'Введите прилагательное. '
         newWord = input(prompt)
         outS = inS.replace(word, newWord, 1)
-    elif word == 'VERB':
+    elif word == words[2]:
         prompt = 'Введите глагол. '
         newWord = input(prompt)
         outS = inS.replace(word, newWord, 1)
-    elif word == 'ADVERB':
+    elif word == words[3]:
         prompt = 'Введите наречие. '
         newWord = input(prompt)
         outS = inS.replace(word, newWord, 1)
     return outS
 
 
-words = ['NOUN', 'ADJECTIVE', 'VERB', 'ADVERB', ]
 outStr = inStr
 for word in words:
     i = 1
