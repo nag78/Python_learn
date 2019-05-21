12776f101e682fa9578edf63289751267a9dc705
Nikolay Goltsev <nag78@yandex.ru>
2019-05-21 17:48:40 +0300

    Update backupToZip.py

--- Al Sweigart/Zip-file/backupToZip.py
+++ Al Sweigart/Zip-file/backupToZip.py
@@ -22,11 +22,24 @@ def backupToZip(folder):
             break
         number = number + 1
 
-        # Создать zip file
-        print('Создается файл %s...' % (zipFilename))
-        backupZip = zipfile.ZipFile(zipFilename, 'w')
+    # Создать zip file
+    print('Создается файл %s...' % (zipFilename))
+    backupZip = zipfile.ZipFile(zipFilename, 'w')
 
-        # TODO: Обойти все дерево папки и
-        # сжать файлы, содержащиеся в
-        # каждой папке.
+    # Обойти все дерево папки и
+    # сжать файлы, содержащиеся в
+    # каждой папке.
+    for foldername, subfolders, filenames in os.walk(folder):
+        print('Добавление файлов из папки %s...' % (foldername))
+        # Добавить в ZIP-файл текущую папку
+        backupZip.write(foldername)
+        # Добавить в ZIP-файл все файлы из этой папки
+        for filename in filenames:
+            if filename.startswith(os.path.basename(folder) + '_') and filename.endswith('.zip'):
+                continue
+            backupZip.write(os.path.join(foldername, filename))
+    backupZip.close()
+    print('Готово')
 
+
+backupToZip('C:\\delicious')
