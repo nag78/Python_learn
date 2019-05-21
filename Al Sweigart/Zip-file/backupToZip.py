7d4930e7544253c46ce78b415f2ef00feadcf078
Nikolay Goltsev <nag78@yandex.ru>
2019-05-21 17:05:12 +0300

    Create backupToZip.py

---
+++ Al Sweigart/Zip-file/backupToZip.py
@@ -0,0 +1,29 @@
+# -*- coding: cp1251 -*-
+
+import zipfile
+import os
+
+
+def backupToZip(folder):
+    # Создание резервной копии всего
+    # содержимого папки folder
+    # в виде Zip файла
+
+    # Определяем что задан абсолютный путь
+    folder = os.path.abspath(folder)
+
+    # Определяем, какое имя должен использо-
+    # вать этот код, исходя из имен уже
+    # существующих файлов
+    number = 1
+    while True:
+        zipFilename = os.path.basename(folder) + '_' + str(number) + '.zip'
+        if not os.path.exists(zipFilename):
+            break
+        number = number + 1
+
+        # TODO: Создать zip file
+        # TODO: Обойти все дерево папки и
+        # сжать файлы, содержащиеся в
+        # каждой папке.
+
