f05559289054782d23c91885c9749895328d895e
Nikolay Goltsev <nag78@yandex.ru>
2019-05-28 14:37:30 +0300

    Create getSizeFile.py

---
+++ Al Sweigart/Zip-file/getSizeFile.py
@@ -0,0 +1,20 @@
+# -*- coding: cp1251 -*-
+# Вывод файлов превышающих 100Мб
+
+import os
+
+
+def getSizeFile(fldr):
+
+    fldr = os.path.abspath(fldr)
+
+    for folder, subfolders, files in os.walk(fldr):
+        for subfolder in subfolders:
+            continue
+
+        for file in files:
+            path = os.path.join(folder, file)
+            if os.path.getsize(path) > 104857600:
+                print(str(file))
+
+getSizeFile('C:\\')
