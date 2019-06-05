a082ab84d3fb88b665e985a1b99b671a3d868be3
Nikolay Goltsev <nag78@yandex.ru>
2019-06-05 18:46:17 +0300

    Update randomGuess.py

--- Al Sweigart/Zip-file/randomGuess.py
+++ Al Sweigart/Zip-file/randomGuess.py
@@ -6,19 +6,19 @@ while guess not in ('орел', 'решка'):
     print('Угадайте результат подбрасывания монеты!')
     guess = input('Орел или решка:')
 toss = random.randint(0, 1)
-if toss == 0:
-    toss = 'решка'
+if guess == 'решка':
+    guess = 0
 else:
-    toss = 'орел'
+    guess = 1
 if toss == guess:
     print('Есть!')
 else:
     print('Увы! Попробуйте ещё раз!')
     guess = input()
-    if toss == 0:
-        toss = 'решка'
+    if guess == 'решка':
+        guess = 0
     else:
-        toss = 'орел'
+        guess = 1
     if toss == guess:
         print('Есть!')
     else:
