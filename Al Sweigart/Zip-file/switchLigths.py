9ac6abf238bc2164c647b4605c7f328c63290511
Nikolay Goltsev <nag78@yandex.ru>
2019-06-05 17:42:31 +0300

    Create switchLigths.py

---
+++ Al Sweigart/Zip-file/switchLigths.py
@@ -0,0 +1,10 @@
+def switchLights(stoplight):
+    for key in stoplight.keys():
+        if stoplight[key] == 'green':
+            stoplight[key] = 'yellow'
+        elif stoplight[key] == 'yellow':
+            stoplight[key] = 'red'
+        elif stoplight[key] == 'red':
+            stoplight[key] = 'green'
+
+switchLights(market_2nd)
