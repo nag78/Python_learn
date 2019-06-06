c90e09694edb79c199da78eeb1aa41600e5bd456
Nikolay Goltsev <nag78@yandex.ru>
2019-06-06 15:58:27 +0300

    Update randomGuess.py

--- Al Sweigart/Zip-file/randomGuess.py
+++ Al Sweigart/Zip-file/randomGuess.py
@@ -1,12 +1,17 @@
 
 import random
+import logging
+logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
+logging.debug('Начало программы')
 
 def brosok(guess, toss):
     '''Функция проверки результата'''
+    logging.debug('Начало подпрограммы.')
     if guess == 'решка':
         guess = 0
     else:
         guess = 1
+    logging.debug('guess = ' + str(guess) + ' toss = ' + str(toss))
     if toss == guess:
         return True
     else:
@@ -17,7 +22,9 @@ guess = ''
 while guess not in ('орел', 'решка'):
     print('Угадайте результат подбрасывания монеты!')
     guess = input('Орел или решка:')
+    logging.debug('guess = ' + str(guess))
 toss = random.randint(0, 1)
+logging.debug('toss = ' + str(toss))
 if brosok(guess, toss):
     print('Есть!')
 else:
@@ -27,7 +34,4 @@ else:
         print('Есть!')
     else:
         print('Нет. Вам действительно не везет в этой игре.')
-
-
-
-# TODO: Дописать логгирование
+logging.debug('Конец программы.')
