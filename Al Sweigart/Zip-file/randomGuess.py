1f240c9e0ce4eac9c71f4858149ec532cf274c5f
Nikolay Goltsev <nag78@yandex.ru>
2019-06-06 15:01:07 +0300

    Update randomGuess.py

--- Al Sweigart/Zip-file/randomGuess.py
+++ Al Sweigart/Zip-file/randomGuess.py
@@ -1,27 +1,33 @@
 
 import random
 
-guess = ''
-while guess not in ('орел', 'решка'):
-    print('Угадайте результат подбрасывания монеты!')
-    guess = input('Орел или решка:')
-toss = random.randint(0, 1)
-if guess == 'решка':
-    guess = 0
-else:
-    guess = 1
-if toss == guess:
-    print('Есть!')
-else:
-    print('Увы! Попробуйте ещё раз!')
-    guess = input()
+def brosok(guess, toss):
+    '''Функция проверки результата'''
     if guess == 'решка':
         guess = 0
     else:
         guess = 1
     if toss == guess:
+        return True
+    else:
+        return False
+
+
+guess = ''
+while guess not in ('орел', 'решка'):
+    print('Угадайте результат подбрасывания монеты!')
+    guess = input('Орел или решка:')
+toss = random.randint(0, 1)
+if brosok(guess, toss):
+    print('Есть!')
+else:
+    print('Увы! Попробуйте ещё раз!')
+    guess = input()
+    if brosok(guess, toss):
         print('Есть!')
     else:
         print('Нет. Вам действительно не везет в этой игре.')
-# TODO: Переписать с функцией
+
+
+
 # TODO: Дописать логгирование
