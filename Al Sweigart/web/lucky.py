30ddca493ae8d5402f6efff7767b95cc2f954da1
Nikolay Goltsev <nag78@yandex.ru>
2019-07-09 18:51:20 +0300

    Update lucky.py

--- Al Sweigart/web/lucky.py
+++ Al Sweigart/web/lucky.py
@@ -1,6 +1,7 @@
+# -*- coding: utf8 -*-
 #! python3
-# lucky.py - Открывает несколько результатов поиска
-# с помощью Google.
+# lucky.py - Открывает несколько результатов
+# поиска с помощью Google.
 
 import requests
 import sys
@@ -8,12 +9,18 @@ import webbrowser
 import bs4
 
 
-print('Гуглим...') # отображается при загрузке гугла
+print('Гуглим...') # отображается при загрузке
+                   # гугла
 res = requests.get('http://google.com/search?q=' +
                     ' '.join(sys.argv[1:]))
 res.raise_for_status()
 
 
-# TODO: Извлечь перввые несколько найденных ссылок
+# Извлечь перввые несколько найденных ссылок
+soup = bs4.BeautifulSoup(res.text, 'lxml')
 
-# TODO: ОТкрыть отдельную вкладку для каждого результата
+# Открыть отдельную вкладку для каждого результата
+linkElems = soup.select('.r a')
+numOpen = min(5, len(linkElems))
+for i in range(numOpen):
+    webbrowser.open('http://google.com' + linkElems[i].get('herf'))
