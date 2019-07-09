d4ef9956e37d3f0071670b66eca454792262ff90
Nikolay Goltsev <nag78@yandex.ru>
2019-07-09 18:11:52 +0300

    Create exampleSoup.py

---
+++ Al Sweigart/web/exampleSoup.py
@@ -0,0 +1,12 @@
+import bs4
+
+
+exampleFile = open('example.html')
+exampleSoup = bs4.BeautifulSoup(exampleFile.read())
+elems = exampleSoup.select('#author')
+type(elems)
+len(elems)
+type(elems[0])
+elems[0].getText()
+str(elems[0])
+elems[0].attrs
