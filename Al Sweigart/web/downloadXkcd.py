e6724dc78a12d9eaf64b339768dc22bde6622e00
Nikolay Goltsev <nag78@yandex.ru>
2019-07-15 12:56:50 +0300

    Update downloadXkcd.py

--- Al Sweigart/web/downloadXkcd.py
+++ Al Sweigart/web/downloadXkcd.py
@@ -1,17 +1,23 @@
+# -*- coding: cp1251 -*-
 #! python3
 # downloadXkcd.py - Загружает все комиксы с XKCD.
 
-import request
+import requests
 import os
 import bs4
 
 
 url = 'http://xkcd.com'
-os.mkdir('xkcd', exist_ok=True)
+os.makedirs('xkcd', exist_ok=True)
 
 while not url.endswith('#'):
-    #TODO: Загрузить страницу
+    print('Загоужается страница %s...' % url)
+    res = requests.get(url)
+    res.raise_for_status()
+
+    soup = bs4.BeautifulSoup(res.text)
     #TODO: Найти URL- адрес изображения комикса
     #TODO: Сохранить картинку в папке ./xkcd.
-    #TODO: Получить URL-адрес кнопки Prev
+    #TODO: Получить URL-адрес кнопки Prev.
+
 print('Готово!')
