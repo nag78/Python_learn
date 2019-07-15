8b208c5a3d41c0dde248d82948102ef1dcacedf4
Nikolay Goltsev <nag78@yandex.ru>
2019-07-15 16:20:44 +0300

    Up

--- Al Sweigart/web/exampleSelenium.py
+++ Al Sweigart/web/exampleSelenium.py
@@ -1,6 +1,16 @@
+# -*- coding: cp1251 -*-
+
+
 from selenium import webdriver
 
 
 browser = webdriver.Firefox(executable_path=r'C:\gecko\geckodriver.exe')
 type(browser)
 browser.get('http://inventwithpython.com')
+
+
+try:
+    elem = browser.find_element_by_class_name('card-img-top')
+    print('Найден элемент <%s> с данным именем класса!' % (elem.tag_name))
+except:
+    print('Не удалось найти элемент с данным именем класса.')
