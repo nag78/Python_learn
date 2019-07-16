24ddd7995b15d0f025a9da679f8eebdc7207d33d
Nikolay Goltsev <nag78@yandex.ru>
2019-07-16 15:14:44 +0300

    Up

---
+++ Al Sweigart/web/test_emoji.py
@@ -0,0 +1,13 @@
+# -*- coding: cp1251 -*-
+
+# from emoji import emojize
+
+# print(emojize(":thumbs_up:").encode("cp1251"))
+import requests
+import pprint
+
+url = 'https://randomuser.me/api/?results=1'
+users = requests.get(url).json()
+users = str(users)
+
+pprint.pprint(users)
