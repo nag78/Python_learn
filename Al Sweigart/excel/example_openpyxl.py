9133e113d64cde3013c1326ec293fbfc541f5cae
Nikolay Goltsev <nag78@yandex.ru>
2019-07-17 14:47:15 +0300

    Update example_openpyxl.py

--- Al Sweigart/excel/example_openpyxl.py
+++ Al Sweigart/excel/example_openpyxl.py
@@ -2,3 +2,14 @@ from openpyxl import load_workbook
 wb = load_workbook(filename = 'example.xlsx')
 sheets = wb.sheetnames
 print(sheets)
+
+sheet = wb[sheets[2]]
+print(sheet)
+ts = type(sheet)
+print(str(ts))
+st = sheet.title
+print(str(st))
+
+anotherSheet = wb.active
+st = anotherSheet.title
+print(str(st))
