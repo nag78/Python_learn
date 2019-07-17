55830dd82fce330ebca5d6d686c5e90692c090e0
Nikolay Goltsev <nag78@yandex.ru>
2019-07-17 12:36:06 +0300

    Update exampleSelenium.py

--- Al Sweigart/web/exampleSelenium.py
+++ Al Sweigart/web/exampleSelenium.py
@@ -8,13 +8,16 @@ browser = webdriver.Firefox(executable_path=r'C:\gecko\geckodriver.exe')
 type(browser)
 browser.get('http://inventwithpython.com')
 
-
+# Поиск объекта на странице
 try:
     elem = browser.find_element_by_class_name('card-img-top')
     print('Найден элемент <%s> с данным именем класса!' % (elem.tag_name))
 except:
     print('Не удалось найти элемент с данным именем класса.')
-
+# Имитация клика мыши
 linkElem = browser.find_element_by_link_text('Read for Free')
 type(linkElem)
 linkElem.click()
+
+# Заполнение полей
+browser.get('gmail.com')
