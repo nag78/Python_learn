ac7d25644a1d01e821bd6b54813d2d902788f7cc
Nikolay Goltsev <nag78@yandex.ru>
2019-07-17 16:27:13 +0300

    New

---
+++ Al Sweigart/excel/readCensusExcel.py
@@ -0,0 +1,26 @@
+# -*- coding: cp1251 -*-
+
+import openpyxl
+from Lib.pprint import pprint
+
+
+print('Открытие рабочей книги...')
+wb = openpyxl.load_workbook('censuspopdata.xlsx')
+sheet = wb.get_sheet_by_name('Population by \
+                             Census Tract')
+countyData = {}
+# TODO: Заполнить словарь countyData данными о
+# TODO: численности населения и
+# TODO: переписных округов.
+
+print('Чтение строк...')
+for row in range(2, sheet.max_row + 1):
+    # В каждой строке содержатся данные
+    # для одного переписного района.
+    state = sheet['B' + str(row)].value
+    conty = sheet['C' + str(row)].value
+    pop = sheet['D' + str(row)].value
+
+# TODO: Открыть новый текстовый файл и
+# TODO: записать в него содержимое
+# TODO: словоря countyData.
