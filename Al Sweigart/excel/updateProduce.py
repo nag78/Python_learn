cc4150b618c1ea1f31e5a3f80fdfc2145a0a2c0c
Nikolay Goltsev <nag78@yandex.ru>
2019-07-18 14:48:50 +0300

    Up Work with Openpyxl

--- Al Sweigart/excel/updateProduce.py
+++ Al Sweigart/excel/updateProduce.py
@@ -4,3 +4,16 @@
 import openpyxl
 
 wb = openpyxl.load_workbook('produceSales.xlsx')
+sheet = wb.active
+
+# Типы продукции и их обновленные цены.
+price_updates = {'Lemon': 3.07,
+                 'Celery': 1.19,
+                 'Garlic': 1.27}
+# Создать цикл по строкам и обновить цены.
+maxRow = sheet.max_row
+for rowNum in range(2, maxRow):
+    produceName = sheet.cell(row=rowNum, column=1).value
+    if produceName in price_updates:
+        sheet.cell(row=rowNum, column=2).value = price_updates[produceName]
+wb.save('updateProduceSales.xlsx')
