a57aca000b3b2141c96e3090c7c49c68184b0315
Nikolay Goltsev <nag78@yandex.ru>
2019-07-23 15:44:02 +0300

    Up

--- Al Sweigart/excel/multiplicationTable.py
+++ Al Sweigart/excel/multiplicationTable.py
@@ -1,27 +1,32 @@
-import os
+import sys
 import openpyxl
 
 # откроем Рабочую книгу
 wb = openpyxl.Workbook()
 ws = wb.active
-# TODO: сделать получение factor через аргумент строки
-factor = 6
-# Разметка таблицы
+# получение factor через аргумент строки
+argument = ''.join(sys.argv[1:])
+if argument == '':
+    print('Нет аргументов строки.')
+else:
+    factor = int(argument)
 
-for i in range(1, factor + 1):
-    _ = ws.cell(row=i+1, column=1, value=i)
-    _ = ws.cell(row=1, column=i+1, value=i)
+    # Разметка таблицы
+
+    for i in range(1, factor + 1):
+        _ = ws.cell(row=i+1, column=1, value=i)
+        _ = ws.cell(row=1, column=i+1, value=i)
 
 
-# Генератор таблицы
-tabl = [[i*j for j in range(1, factor + 1)] for i in range(1, factor + 1)]
+    # Генератор таблицы
+    tabl = [[i*j for j in range(1, factor + 1)] for i in range(1, factor + 1)]
 
-col = 2
-row = 2
-for subtabl in tabl:
-    for index, value in enumerate(subtabl):
-        ws.cell(column=col+index, row=row).value = value
-    row += 1
+    col = 2
+    row = 2
+    for subtabl in tabl:
+        for index, value in enumerate(subtabl):
+            ws.cell(column=col+index, row=row).value = value
+        row += 1
 
-filename = 'multiple' + str(factor) + '.xlsx'
-wb.save(filename)
+    filename = 'multiple' + str(factor) + '.xlsx'
+    wb.save(filename)
