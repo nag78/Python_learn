792450792a13dc4ec45c3ae5f9a3bfb369f61033
Nikolay Goltsev <nag78@yandex.ru>
2019-07-25 12:47:59 +0300

    Epic win!

--- Al Sweigart/excel/inversTable.py
+++ Al Sweigart/excel/inversTable.py
@@ -21,13 +21,14 @@ tabl = [[ws.cell(column=i, row=j).value
         for j in range(1, rows)] for i in range(1, cols)]
 print(tabl)
 
-tabl = list(zip(*tabl))
+tabl1 = list(zip(*tabl))
+print(tabl1)
 col = 1
 row = 1
-for subtable in tabl:
+for subtable in tabl1:
     for index, value in enumerate(subtable):
-        ws1.cell(column=col+index, row=row).value = value
-    row += 1
+        ws1.cell(column=col, row=row+index).value = value
+    col += 1
 
 
 fileout = 'inv' + filein
